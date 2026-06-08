f51d2bf0aad366a485073abe44d7ab41c11c0e03
AZZIRARI-ABDESSAMAD <azzirari-abdessamad@users.noreply.github.com>
2026-06-08 16:06:30 +0100

    Fix user registration and test suite for role-based system

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,8 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'chef_espace',
+            'phone' => fake()->phoneNumber(),
             'remember_token' => Str::random(10),
         ];
     }
